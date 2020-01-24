fda0f42486d182c195d54b51be61381cf96ed90e
Sasha <oleksandr.holdfarb@gmail.com>
2020-01-24 10:52:44 -0300

    Updates

---
+++ precache-manifest.2a7ed2c89da9527e9db4a623d0fbd47e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02df24bdb356fa4a6a81042944368da9",
+    "url": "/react-form-hooks-playground/index.html"
+  },
+  {
+    "revision": "2e722a2daecfd7a922ab",
+    "url": "/react-form-hooks-playground/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "21b50817507d00fae97f",
+    "url": "/react-form-hooks-playground/static/js/2.807c1340.chunk.js"
+  },
+  {
+    "revision": "2e722a2daecfd7a922ab",
+    "url": "/react-form-hooks-playground/static/js/main.b3a9ef0b.chunk.js"
+  },
+  {
+    "revision": "ff564d046a9dfa073e2d",
+    "url": "/react-form-hooks-playground/static/js/runtime-main.e115f6fd.js"
+  }
+]);
